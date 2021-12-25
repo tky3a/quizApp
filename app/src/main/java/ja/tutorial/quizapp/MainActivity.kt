24fc7e4c405ae09c5604eabefecc3df6f6f158b0
tky <takayamac@icloud.com>
2021-12-25 17:21:53 +0900

    bindingの準備と必要画像の追加

--- app/src/main/java/ja/tutorial/quizapp/MainActivity.kt
+++ app/src/main/java/ja/tutorial/quizapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package ja.tutorial.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ja.tutorial.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
